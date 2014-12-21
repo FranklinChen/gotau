7e96951b59e80b0fd5e2eb326ceb932fa1682ab3
Franklin Chen <franklinchen@franklinchen.com>
2014-12-21 01:14:31 -0500

    Support Go 1.4 in Travis.

--- tau.go
+++ tau.go
@@ -4,5 +4,5 @@ package gotau
 // ASCII version of the mathematical constant Tau, where Pi = Tau/2.
 const Tau = 6.2831853071795864769252867665590057683943387987502116419498892
 
-// Unicode version of the mathematical constant Tau, where π = τ/2.
+// Unicode version of the mathematical constant τ, where π = τ/2.
 const τ = Tau
